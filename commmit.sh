225b70b789c72de2b0e815056882ffff575be07a
Leonid Kaganov <lleo@lleo.me>
2025-07-30 23:21:15 +0300

    Add /api2/insert /api2/update; fix migrate; fix uuid

--- commmit.sh
+++ commmit.sh
@@ -2,7 +2,10 @@
 
 clear
 
-NAME="insert_update_uuid"
+#git push origin --delete feature/insert_update
+#git push origin --delete feature/insert_update_uuid
+
+NAME="insert_update"
 
 git checkout -b feature/${NAME}
 git add .
